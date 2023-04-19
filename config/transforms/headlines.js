a70b0220f3db4e38a29ea51ca2068591fa4d332e
Lea Rosema <lea@lea.codes>
2023-04-19 23:11:05 +0200

    chore: fix formatting

--- config/transforms/headlines.js
+++ config/transforms/headlines.js
@@ -1,12 +1,12 @@
 // by Vadim Makeev github.com/pepelsbey/pepelsbey.dev
-module.exports = function(window) {
-	const document = window.document;
+module.exports = function (window) {
+  const document = window.document;
   const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 
   const slugify = text => text.trim().toLowerCase().replace(/[,\.:!?\-\;]/g, '').replace(/[\s,\.:!?\-\;]/g, '-');
-	for (const heading of headings) {
-		const text = heading.textContent.trim()
-		const id = slugify(text).toLowerCase();
-		heading.setAttribute('id', id);
-	}
+  for (const heading of headings) {
+    const text = heading.textContent.trim()
+    const id = slugify(text).toLowerCase();
+    heading.setAttribute('id', id);
+  }
 }
