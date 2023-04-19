eef906e590d6c17ee7ec29be71d0e3bfc437c05d
Lea Rosema <lea@lea.codes>
2023-04-19 23:06:22 +0200

    fix: fix html issues

--- config/transforms/headlines.js
+++ config/transforms/headlines.js
@@ -3,7 +3,7 @@ module.exports = function(window) {
 	const document = window.document;
   const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 
-  const slugify = text => text.trim().toLowerCase().replace(/\s/g, '-');
+  const slugify = text => text.trim().toLowerCase().replace(/[,\.:!?\-\;]/g, '').replace(/[\s,\.:!?\-\;]/g, '-');
 	for (const heading of headings) {
 		const text = heading.textContent.trim()
 		const id = slugify(text).toLowerCase();
