6578294e04a9ab75d02847da2d13652444ec34b8
Lea Rosema <lea@lea.codes>
2023-04-23 16:59:46 +0200

    fix: fix skip link

--- config/transforms/headlines.js
+++ config/transforms/headlines.js
@@ -1,12 +1,19 @@
 // by Vadim Makeev github.com/pepelsbey/pepelsbey.dev
-module.exports = function (window) {
+module.exports = function (window, _content, path) {
   const document = window.document;
   const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
 
-  const slugify = text => text.trim().toLowerCase().replace(/[,\.:!?\-\;]/g, '').replace(/[\s,\.:!?\-\;]/g, '-');
+  const slugify = text => text.trim().toLowerCase().replace(/[^a-zA-Z0-9\s]/g, '').replace(/\s/g, '-');
   for (const heading of headings) {
-    const text = heading.textContent.trim()
+    const text = heading.textContent.trim();
     const id = slugify(text).toLowerCase();
     heading.setAttribute('id', id);
   }
+
+  const skipLink = document.querySelector('.skip-link');
+  const h1 = document.querySelector('h1');
+  if (! h1) {
+    throw new Error('no H1 detected in ' + path);
+  }
+  skipLink.setAttribute('href', `#${h1.getAttribute('id')}`);
 }
