6af698e8b5bac20f086f3bd952fbeddbf74c540f
Lea Rosema <lea@lea.codes>
2023-05-08 14:45:04 +0200

    refactor: use html template literals with vanillaH

--- config/transforms/headlines.js
+++ config/transforms/headlines.js
@@ -1,38 +1,32 @@
+const vanillaH = require('vanillah');
+const htm = require('htm');
+
 // Code for adding id's to headlines is by Vadim Makeev github.com/pepelsbey/pepelsbey.dev
 // heading wrapper idea is from https://11ty.rocks by Stephanie Eckles.
 module.exports = function (window, _content, path) {
   const document = window.document;
+
+  const h = vanillaH(document);
+  const html = htm.bind(h);
+
   const headings = document.querySelectorAll('h1, h2, h3, h4, h5, h6');
   const isPost = /posts[\/\\]/.test(path);
 
   const article = document.querySelector('.content > article');
 
-  function create(tagName, attribs, children) {
-    const el = document.createElement(tagName);
-    for (const [attrib, value] of Object.entries(attribs)) {
-      el.setAttribute(attrib, value);
-    }
-    for (const child of children) {
-      el.appendChild(child);
-    }
-    return el;
-  }
-
   function wrapHeading(heading) {
     const id = heading.getAttribute('id');
-    const headingWrapper = create('div', {
-      'class': [
-        'heading-wrapper',
-        heading.nodeName.toLowerCase(),
-        heading.getAttribute('class')].join(' '),
-    }, [
-      create('a', {
-        'href': '#' + id,
-        'aria-labelledby': id,
-      }, [
-        create('span', {'aria-hidden': true}, [document.createTextNode('#')])
-      ]),
-    ]);
+    const tag = heading.nodeName.toLowerCase();
+    const classes = heading.getAttribute('class');
+
+    const headingWrapper = html`
+      <div class="heading-wrapper ${tag} ${classes}">
+        <a href="#${id}" aria-labelledBy="${id}">
+          <span aria-hidden="true">#</span>
+        </a>
+      </div>
+    `;
+
     heading.parentNode.insertBefore(headingWrapper, heading);
     headingWrapper.insertBefore(heading, headingWrapper.firstChild);
   }
